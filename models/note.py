fd36505130c19ec1ea8f6dd6f1841f887dbe7d8f
pOsdas <ratmir1099@mail.ru>
2024-11-30 01:18:10 +0300

    minor fix

--- models/note.py
+++ models/note.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from datetime import datetime
 
 from models.base import Base
 
@@ -12,14 +11,14 @@ class Note(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     status = Column(String, nullable=False)
-    created_date = Column(DateTime, default=datetime.utcnow)
-    issued_date = Column(DateTime, nullable=True)
+    created_date = Column(String, nullable=False)
+    issue_date = Column(String, nullable=False)
 
     # one to many connection
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     user = relationship('User', back_populates='notes')
 
-    def __str__(self):
+    def __repr__(self):
         return (
             f"<Note(id={self.id}, title={self.title}>, status={self.status}, "
             f"created_date={self.created_date}, issue_date={self.issue_date}, user_id={self.user_id})>"
