d421bf3ebd5a7883d4dfcb640ba15e117ab5459c
pOsdas <ratmir1099@mail.ru>
2024-12-08 01:03:36 +0300

    create compare dates func

--- utils/date_validator.py
+++ utils/date_validator.py
@@ -10,6 +10,25 @@ def format_date(date: str) -> datetime:
         raise ValueError(f"Не удалось обработать ввод даты: {date}")
 
 
+def compare_dates(issue_date: datetime, status: str) -> str:
+    current_time = datetime.now()
+
+    if status == "Отложено" or status == "Готово":
+        return ""
+    if current_time < issue_date:
+        remaining_days = issue_date - current_time
+        days = remaining_days.days
+        hours, remainder = divmod(remaining_days.seconds, 3600)
+        minutes, _ = divmod(remainder, 60)
+        return f"До дедлайна осталось {days} дней, {hours} часов, {minutes} минут."
+    elif current_time >= issue_date:
+        overdue_days = current_time - issue_date
+        days = overdue_days.days
+        hours, remainder = divmod(overdue_days.seconds, 3600)
+        minutes, _ = divmod(remainder, 60)
+        return f"Дедлайн просрочен на {days} дней, {hours} часов, {minutes} минут."
+
+
 # transfer from str to datetime object
 # def handle_date(value):
 #     if isinstance(value, str) and value:
