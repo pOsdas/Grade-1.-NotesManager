f916d9a96a58ca8bd4e361118e5a806844e68b8f
pOsdas <ratmir1099@mail.ru>
2025-01-16 23:40:51 +0300

    add db_helper.py

---
+++ database/db_helper.py
@@ -0,0 +1,49 @@
+from sqlalchemy.sql import text
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models.note import Note
+
+DATABASE_URL = 'sqlite:///../notes_manager.db'
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+session = SessionLocal()
+
+
+# Просмотреть структуру таблицы
+def inspect_notes_table():
+    try:
+        result = session.execute(text("PRAGMA table_info(notes)")).fetchall()
+        print("Структура таблицы notes:")
+        for row in result:
+            print(row)
+    except Exception as e:
+        print(f"Ошибка при проверке структуры таблицы: {e}")
+    finally:
+        session.close()
+
+
+# Получить все заметки
+def fetch_all_notes_raw():
+    try:
+        result = session.execute(text("SELECT * FROM notes")).fetchall()
+        if result:
+            print("Содержимое таблицы notes:")
+            for row in result:
+                print(row)
+        else:
+            print("Таблица notes пуста.")
+    except Exception as e:
+        print(f"Ошибка при выполнении запроса: {e}")
+    finally:
+        session.close()
+
+
+# inspect_notes_table()
+fetch_all_notes_raw()
