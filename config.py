a1a894834b2b38a8f79e809c0bff5fe570fb985b
pOsdas <ratmir1099@mail.ru>
2025-01-21 01:32:15 +0300

    added config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# main settings here
+
+DATABASE_URL = 'sqlite:///notes_manager.db'  # Путь к базе данных
+export_dir_path = "../exports"  # Путь директории для экспорта
+export_path_txt = "../exports/notes_export.txt"  # Путь для экспорта заметок в .txt
+export_path_pdf = "../exports/notes_export.pdf"  # Путь для экспорта заметок в .pdf
